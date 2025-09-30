1d434d284210244e7299f11ec834f78f0f6f0e29
devolexa <starovatovasasha@gmail.com>
2025-09-30 11:57:59 +0300

    objects

---
+++ main.js/objects.js/objects.js
@@ -0,0 +1,35 @@
+const colors = [
+    { hex: "#f44336", rgb: "244,67,54" },
+    { hex: "#2196f3", rgb: "33,150,243" },
+    { hex: "#4caf50", rgb: "76,175,80" },
+    { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+for (const color of colors) {
+    hexColors.push(color.hex);
+    rgbColors.push(color.rgb);
+}
+
+console.log(hexColors);
+console.log(rgbColors);
+
+//!Функція calculateTotalPrice(productName) приймає один параметр productName- назва товару. Функція містить масив об'єктів products з такими властивостями, як name — ім'я товару, price — ціна і quantity — кількість. Доповни код функції так, щоб вона повертала загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
+function calculateTotalPrice(productName) {
+    const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+    ];
+    let totalPrice = 0;
+    for (const product of products) {
+    if (product.name === productName) {
+      totalPrice = product.price * product.quantity;
+        return totalPrice
+    }
+    }
+    return `Product ${productName} not found!`
+}
